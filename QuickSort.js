562e906fc91bef509e147c9d31622ed644f04075
Joonhyeok Im <itanoss@gmail.com>
2017-05-18 14:56:21 +0900

    Update QuickSort.js
    Convert tabs to spaces

--- QuickSort.js
+++ QuickSort.js
@@ -1,27 +1,27 @@
 var quickSort = function(array) {
-	var swap = function(a, b) {
+    var swap = function(a, b) {
         var buf = array[a];
         array[a] = array[b];
         array[b] = buf;
     };
 
     var partition = function(p, r) {
-    	var q = p;
-    	for(var j = p; j < r; ++j) {
-    		if(array[j] <= array[r]) {
-    			swap(j, q++);
-    		}
-    	}
-    	swap(r, q);
-    	return q;
+        var q = p;
+        for(var j = p; j < r; ++j) {
+            if(array[j] <= array[r]) {
+                swap(j, q++);
+            }
+        }
+        swap(r, q);
+        return q;
     };
 
     var execute = function(p, r) {
-    	if(p < r) {
-    		var q = partition(p, r);
-    		execute(p, q-1);
-    		execute(q+1, r);
-    	}
+        if(p < r) {
+            var q = partition(p, r);
+            execute(p, q-1);
+            execute(q+1, r);
+        }
     }
 
     execute(0, array.length-1);
